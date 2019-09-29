e3071f81a37c5b03551428a08dcc2e6fcaafbc7a
arnaud ferrand <arferrand@gmail.com>
2019-09-29 16:11:50 +0100

    removes commented code

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -3,54 +3,8 @@ import PropTypes from 'prop-types';
 import InputAPIKey from './InputAPIKey';
 import InputAddresses from './InputAddresses';
 import AddressesTable from './AddressesTable';
-// import api from '../services/api';
 
 class SideBar extends Component {
-  // constructor(props) {
-  //   super(props);
-  // this.state = {
-  //   apiKey: props.apiLey,
-  //   addresses: props.addresses,
-  //   addressesDisbaled: props.addressesDisbaled,
-  //   submitDisbaled: props.submitDisbaled,
-  //   results: props.results,
-  // };
-  // this.handleAPIKeyChange = props.onAPIKeyChange;
-  // this.handleAddressesChange = props.onAddressesChange;
-  // this.handleSubmit = props.onSubmit;
-  // this.handleRestAddresses = props.onRestAddresses;
-  // }
-  // handleAPIKeyChange(event) {
-  //   const { value } = event.target;
-  //   let disabled = true;
-  //   if (value.length >= 30) {
-  //     disabled = false;
-  //   }
-  //   this.setState({ apiKey: value, addressesDisbaled: disabled });
-  // }
-
-  // handleAddressesChange(event) {
-  //   const { value } = event.target;
-  //   let disabled = true;
-  //   if (value.length >= 2) {
-  //     disabled = false;
-  //   }
-  //   this.setState({ addresses: value, submitDisbaled: disabled });
-  // }
-
-  // handleSubmit(event) {
-  //   event.preventDefault();
-  //   const inarray = this.state.addresses.split('\n');
-  //   api.batchGeocode(this.state.apiKey, inarray).then(data => {
-  //      console.log(data);
-  //     this.setState({ results: data, submitDisbaled: true, addressesDisbaled: true });
-  //   });
-  // }
-
-  // handleRestAddresses(event) {
-  //   this.setState({ results: [], addresses: '', addressesDisbaled: false });
-  // }
-
   render() {
     const {
       addressesDisbaled,
